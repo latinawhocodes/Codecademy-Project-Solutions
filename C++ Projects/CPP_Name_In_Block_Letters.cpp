c11f4486e1303fe457826b234e8e6039ac365dec
latinawhocodes <alyssaroque01@gmail.com>
2019-12-02 16:16:15 -0500

    Block Letters Project Solution

---
+++ C++ Projects/CPP_Name_In_Block_Letters.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+int main(){
+    std::cout <<  "     A     \n";
+    std::cout <<  "    A A    \n";
+    std::cout <<  "   AAAAA   \n";
+    std::cout <<  "  A     A  \n";
+    std::cout <<  " A       A \n";
+}
