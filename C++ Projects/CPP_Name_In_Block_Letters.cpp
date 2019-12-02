06a8158005648d713cb751ddf4f170f8cbc62e86
latinawhocodes <alyssaroque01@gmail.com>
2019-12-02 16:18:31 -0500

    Update Block Letter Project C++

--- C++ Projects/CPP_Name_In_Block_Letters.cpp
+++ C++ Projects/CPP_Name_In_Block_Letters.cpp
@@ -1,9 +1,18 @@
 #include <iostream>
 
 int main(){
-    std::cout <<  "     A     \n";
-    std::cout <<  "    A A    \n";
-    std::cout <<  "   AAAAA   \n";
-    std::cout <<  "  A     A  \n";
-    std::cout <<  " A       A \n";
+    std::cout <<  "     A       \n";
+    std::cout <<  "    A A      \n";
+    std::cout <<  "   AAAAA     \n";
+    std::cout <<  "  A     A    \n";
+    std::cout <<  " A       A   \n";
+
+    std::cout <<  "  Y      Y   \n";
+    std::cout <<  "    Y   Y    \n";
+    std::cout <<  "      Y      \n";
+    std::cout <<  "      Y      \n";
+    std::cout <<  "      Y      \n";
+
 }
+
+//Alyssa Yesilyurt
