d1eea2664788ffa5d481f8967307e75f570d727c
latinawhocodes <alyssaroque01@gmail.com>
2019-12-04 20:51:49 -0500

    Update CPP_Leap_Year.cpp

--- C++ Projects/CPP_Leap_Year.cpp
+++ C++ Projects/CPP_Leap_Year.cpp
@@ -6,28 +6,28 @@ int main(){
     int year;
 
     std::cout << "Please enter the year: ";
-    std::cin << year;
+    std::cin >> year;
 
-    if (year <= 1000){
+    if (year >= 1000){
         std::cout << year;
     }
     else {
-        std::cout << "Invalid!";
+        std::cout << "\nInvalid!";
     }
 
     if (year % 100 == 0 && year % 400 == 0){
-        std::cout << "Leap Year";
+        std::cout << "\nLeap Year";
     }
     else if (year % 400 == 0){
-        std::cout << "Leap Year";
+        std::cout << "\nLeap Year";
     }
     else if (year % 100 == 0 && year % 400 != 0){
-        std::cout << "Not a Leap Year!";
+        std::cout << "\nNot a Leap Year!";
     }
     else if (year % 4 == 0){
-        std::cout << "Leap Year";
+        std::cout << "\nLeap Year";
     }
     else {
-        std::cout << "Not a Leap Year!";
+        std::cout << "\nNot a Leap Year!";
     }
 }
