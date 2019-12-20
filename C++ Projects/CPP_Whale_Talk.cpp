bcd345c068bc5e66b6a6382a5e263b7d64fe4be2
latinawhocodes <alyssaroque01@gmail.com>
2019-12-20 09:49:29 -0500

    Create CPP_Whale_Talk.cpp

---
+++ C++ Projects/CPP_Whale_Talk.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <vector>
+#include <string>
+
+int main(){
+
+    std::string input = "turpentine and turtles";
+    std::vector<char> vowels = {"a", "e","i","o","u"};
+    std::vector<char> result;
+
+    for (int i = 0; i < input.size(); i++){
+        for (int k = 0; i < vowels.size(); k++){
+            if (input[i] == vowels[k]){
+                result.push_back(input[i]);
+                if (input[i] == 'e' || input[i] == 'u'){
+                    result.push_back(input[i]);
+                }
+            }
+        }
+    }
+
+    for (int m = 0; m < result.size(); m++){
+        std::cout << result[k];
+    }
+    std::cout << "\n";
+
+
+
+}
