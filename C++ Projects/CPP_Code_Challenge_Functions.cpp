cec4d0e221790b9902ca78cb4cdaf725d57009fb
latinawhocodes <alyssaroque01@gmail.com>
2019-12-22 22:47:38 -0500

    Create CPP_Code_Challenge_Functions.cpp

---
+++ C++ Projects/CPP_Code_Challenge_Functions.cpp
@@ -0,0 +1,64 @@
+#include <iostream>
+#include <vector>
+
+//Challenge 1
+void introduction(std::string first_name, std::string last_name){
+    std::cout << last_name << ", " << first_name << " " << last_name;
+}
+    //output: Bond, James Bond
+
+//Challenge 2
+double average(double num1, double num2){
+    return (num1 + num2)/2;
+}
+    //output: 33
+
+//Challenge 3
+int tenthPower(int num){
+    return pow(num, 10);
+}
+    //output: 1024
+
+//Challenge 4
+std::vector<int> firstThreeMultiples(int num){
+    std::vector<int> multiples{num, num * 2, num * 3};
+    return multiples;
+}
+    //output: 8, 16, 24
+
+
+//Challenge 5
+std::string needsWater(int days, bool isSucculent){
+    if (isSucculent == false && days > 3){
+        return "Time to water the plant.";
+    } else if (isSucculent == true && days <= 12){
+        return "Don't water the plant!";
+    } else if (isSucculent == true && days >= 13){
+        return "Go ahead and give the plant a little water.";
+    } else {
+        return "Don't water the plant!";
+    }
+}
+
+int main(){
+
+    //Challenge 1
+    introduction("James", "Bond");
+
+    //Challenge 2
+    average(42.0, 24.0);
+
+    //Challenge 3
+    tenthPower(2);
+
+    //Challenge 4
+    for (int element : firstThreeMultiples(8)){
+        std::cout << element << "\n";
+    }
+
+    //Challenge 5
+    std::cout << needsWater(10, false) << "\n";
+}
+
+
+
