e224b7d2181f4cc70408843d5038221491091e46
latinawhocodes <alyssaroque01@gmail.com>
2019-12-28 21:36:19 -0500

    Update app.cpp

--- C++ Projects/CPP_Object_Of_Affection_Project/app.cpp
+++ C++ Projects/CPP_Object_Of_Affection_Project/app.cpp
@@ -6,6 +6,12 @@ int main(){
 
     Profile sam("Sam Drakkila", 30, "New York", "USA", "he/him");
 
+    sam.add_hobby("listening to audiobooks and podcasts");
+    sam.add_hobby("playing sports like bowling and kickball");
+    sam.add_hobby("writing a fiction novel");
+    sam.add_hobby("reading advice columns");
+
     std::cout << sam.view_profile();
+
 
 }
